675480b8cd1cb1dbbc024b4a6eee7e1f50bdb126
logo448 <stenzellogan@gmail.com>
2014-11-05 21:42:10 -0600

    Made some improvements to perf_mon. Still works

--- Per_Mon/Program.cs
+++ Per_Mon/Program.cs
@@ -12,34 +12,43 @@ namespace Per_Mon
 {
     class Program
     {
-        static void Main(string[] args)
-        {           
-            #region initalize_objects
-            // create performence monitor objects
-            PerformanceCounter cpu_perc = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
-            PerformanceCounter mem_open = new PerformanceCounter("Memory", "Available MBytes");
-            PerformanceCounter uptime = new PerformanceCounter("System", "System Up Time");
+        #region initalize_objects
+        // create performence monitor objects
+        public static PerformanceCounter cpu_perc = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+        public static PerformanceCounter mem_open = new PerformanceCounter("Memory", "Available MBytes");
+        public static PerformanceCounter uptime = new PerformanceCounter("System", "System Up Time");        
 
+        // initailize speech object
+        public static SpeechSynthesizer synth = new SpeechSynthesizer();
+
+        // create random object
+        public static Random rand = new Random();
+
+        #endregion
+
+        static void Main(string[] args)
+        {
             // initailize uptime
             uptime.NextValue();
 
-            // initailize speech object
-            SpeechSynthesizer synth = new SpeechSynthesizer();
-
-            // create random object
-            Random rand = new Random();
-            #endregion
+            // assign system uptime to a temp variable
+            string uptime_string = uptime_to_string();
 
             // welcome user via print and audio
             string welcome = "Welcome to System Monitor! By: Logan Stenzel";
             Console.WriteLine(welcome);
             synth.Speak(welcome);
-            Console.WriteLine(uptime_to_string(uptime));
+            Console.WriteLine(uptime_string);
+            synth.Speak(uptime_string);
 
             // variable to keep track of elapsed time
             int elapsed_time = 0;
 
-            //infinite while loop
+            //variables to see how many times cpu percent has gone above certain thresholds
+            int eighty_percent_tracker = 0;
+            int hundred_percent_tracker = 0;
+
+            // infinite while loop
             while (true) 
             {
                 // get current stats
@@ -55,6 +64,7 @@ namespace Per_Mon
                 Console.WriteLine(mem_string);
                 Console.WriteLine();
 
+                #region time_logic
                 // increment time variable by 5
                 elapsed_time = elapsed_time + 5;
 
@@ -62,23 +72,52 @@ namespace Per_Mon
                 if (elapsed_time % 3600 == 0)
                 {
                     // tell user that one hour has elapsed
-                    Console.WriteLine(uptime_to_string(uptime));
+                    Console.WriteLine(uptime_to_string());
                     Console.WriteLine();
                     synth.Speak("one hour has elapsed");
                 }
 
+                // check to see if program has been running for about 15 minutes
+                if (elapsed_time % 900 == 0)
+                {
+                    // speak current stats
+                    synth.Speak(cpu_string);
+                    synth.Speak(mem_string);
+
+                    // reset eighty and hundred percent trackers every 15 minutes
+                    eighty_percent_tracker = 0;
+                    hundred_percent_tracker = 0;
+                }
+                #endregion
+
+                #region cpu_logic
+                // check to see if cpu usage is over 80 percent and has been repeated less then 5 times
+                if (cpu_val > 80 && eighty_percent_tracker < 5)
+                {
+                    synth.Speak(cpu_string);
+                    eighty_percent_tracker++;
+                }
+
+                // check to see if cpu usage is 100 percent and has been repeated less then 7 times
+                if (cpu_val == 100 && hundred_percent_tracker < 7)
+                {
+                    synth.Speak("Holy Balls");
+                    hundred_percent_tracker++;
+                }
+                #endregion
+
                 // pause 5 seconds
-                Thread.Sleep(5000);
+                Thread.Sleep(2500);
             }
         }
         /// <summary>
         /// convert the system uptime in seconds to d/h/m/s
         /// </summary>
         /// <returns></returns>
-        static string uptime_to_string(PerformanceCounter uptime)
+        static string uptime_to_string()
         {
             TimeSpan uptime_val = TimeSpan.FromSeconds(uptime.NextValue());
-            string uptime_string = string.Format("Current system up time: {0}D/{1}H/{2}M/{3}S",
+            string uptime_string = string.Format("Current system up time: {0} Days {1} Hours {2} Minutes {3} Seconds",
                 uptime_val.Days,
                 uptime_val.Hours,
                 uptime_val.Minutes,
