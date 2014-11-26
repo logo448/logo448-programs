fa4e697e8112f118e4a1a4a2ee90e8c80ba7f485
logo448 <stenzellogan@gmail.com>
2014-11-26 15:21:52 -0600

    Added a prime generator library. No functionality currently.

---
+++ Prime_Generator_Beta_V1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Generator_Beta_V1
+{
+    public class generate
+    {
+
+    }
+}
