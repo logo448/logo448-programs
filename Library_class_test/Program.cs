981ed49f55dbda7e47a953d381f8a7ac1e697b3f
Logan-Msi <stenzellogan@gmail.com>
2014-11-26 20:53:37 -0600

    Prime generator dill is working. Added a project to test c# class libraries in development.

---
+++ Library_class_test/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Prime_Generator_Beta_V1;
+
+namespace Library_class_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(generate_primes.start_generate(100000000).Capacity);
+        }
+    }
+}
